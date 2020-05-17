6e8c9311b3acc61c247adda5000cfcd89cce2251
Alfredo Stochiero <alfredo_stochiero@hotmail.com>
2020-05-17 09:41:38 -0300

    Tetrimonoes created

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded',function(){
+    
+    for(let i=0;i<200;i++){
+        var tag = document.createElement('div');
+        var texto =  document.createTextNode('*');
+        var element = document.getElementById('grid');
+        tag.appendChild(texto);
+        element.appendChild(tag);
+    }
+    
+    const grid =  document.querySelector('.grid'); // getting a single element with class grid
+    let squares =  Array.from(document.querySelectorAll('.grid div')); // getting all elements with class grid and converting it into an array
+    const ScoreDisplay =  document.getElementById('score');
+    const StartBtn =  document.getElementById('start-button')
+    const width = 10; // define a const that represents the width of the square
+    
+    // The Forms AKA Tetrominoes
+    // each item in the array represents the index of the div corresponding to the square that makes the Tetromino
+
+    const LTetromino = [
+        [1,width+1,width*2+1,2],             // indexes 1,11,21,2
+        [width,width+1,width+2,width*2+2],   // indexes 10,11,12,22
+        [1,width+1,width*2+1,width*2],       // indexes 1,11,21,20
+        [width, width*2,width*2+1,width*2+2] // index 10,20,21,22
+    ]
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+    ]
+    
+      const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+    
+      const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]
+    
+      const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+    const theTetrominoes = [LTetromino,zTetromino,tTetromino,oTetromino,iTetromino];
+
+    let currentPosition = 4
+    let current =  theTetrominoes[0][0]
+
+    // continue on 40:40 https://www.youtube.com/watch?v=rAUn1Lom6dw&t=21s
+
+
+
+
+
+})
